2a2883413ea9b26c53f91014ec57e6c668a5506c
Xen Abano <xeniamae.abano@gmail.com>
2021-10-28 22:20:18 +0800

    change 6

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ export default function AboutMe(){
             <li>First Name: Xen</li>
             <li>Last Name: Abano</li>
             <li>Age: 21</li>
-            <li>Interest: Art, Music, Food</li>
+            <li>Interest: Art</li>
         </ul>
         </Jumbotron>
     )
